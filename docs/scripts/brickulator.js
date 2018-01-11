1b0cb086e2352873a56eed9738613ef8991ca8b3
Kevin Powell <kevinmpowell@gmail.com>
2018-01-10 22:38:41 -0500

    Further work on off canvas menu

--- docs/scripts/brickulator.js
+++ docs/scripts/brickulator.js
@@ -927,50 +927,6 @@ BC.SetSummary = function() {
 }();
 
 'use strict';
-BC.SetSummary = function() {
-  const showMenuSelector = '.bc-site-menu-show-trigger',
-        hideMenuSelector = '.bc-site-menu-hide-trigger',
-        menuSelector = '.bc-site-menu',
-        menuVisibleClass = 'bc-site-menu--visible';
-
-  let showMenuTriggers,
-      hideMenuTriggers,
-      menu;
-
-  const showMenu = function showMenu() {
-    menu.classList.add(menuVisibleClass);
-  }
-
-  const hideMenu = function showMenu() {
-    menu.classList.remove(menuVisibleClass);
-  }
-
-  function setEventListeners() {
-    showMenuTriggers.forEach(function(t){
-      t.addEventListener("click", showMenu);
-    });
-
-    hideMenuTriggers.forEach(function(t){
-      t.addEventListener("click", hideMenu);
-    });
-  }
-
-
-  const initialize = function initialize() {
-    showMenuTriggers = Array.from(document.querySelectorAll(showMenuSelector));
-    hideMenuTriggers = Array.from(document.querySelectorAll(hideMenuSelector));
-    menu = document.querySelector(menuSelector);
-    setEventListeners();
-  }
-
-  return {
-    initialize: initialize,
-    showMenu: showMenu,
-    hideMenu: hideMenu,
-  }
-}();
-
-'use strict';
 BC.SignUpForm = function() {
   const signUpFormId = 'bc-sign-up-form',
         emailFieldId = 'bc-sign-up-form-email',
@@ -1061,5 +1017,49 @@ BC.SignUpForm = function() {
 
   return {
     initialize: initialize
+  }
+}();
+
+'use strict';
+BC.SetSummary = function() {
+  const showMenuSelector = '.bc-site-menu-show-trigger',
+        hideMenuSelector = '.bc-site-menu-hide-trigger',
+        menuSelector = '.bc-site-menu',
+        menuVisibleClass = 'bc-site-menu--visible';
+
+  let showMenuTriggers,
+      hideMenuTriggers,
+      menu;
+
+  const showMenu = function showMenu() {
+    menu.classList.add(menuVisibleClass);
+  }
+
+  const hideMenu = function showMenu() {
+    menu.classList.remove(menuVisibleClass);
+  }
+
+  function setEventListeners() {
+    showMenuTriggers.forEach(function(t){
+      t.addEventListener("click", showMenu);
+    });
+
+    hideMenuTriggers.forEach(function(t){
+      t.addEventListener("click", hideMenu);
+    });
+  }
+
+
+  const initialize = function initialize() {
+    showMenuTriggers = Array.from(document.querySelectorAll(showMenuSelector));
+    hideMenuTriggers = Array.from(document.querySelectorAll(hideMenuSelector));
+    menu = document.querySelector(menuSelector);
+    setEventListeners();
+  }
+
+  return {
+    initialize: initialize,
+    showMenu: showMenu,
+    hideMenu: hideMenu,
   }
 }();
