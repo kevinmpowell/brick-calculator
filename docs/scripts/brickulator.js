bc2057be786ba797e4473e6d60d2b9662a98c1a3
Kevin Powell <kevinmpowell@gmail.com>
2018-01-26 00:54:19 -0600

    Add favicons for everything

--- docs/scripts/brickulator.js
+++ docs/scripts/brickulator.js
@@ -1140,6 +1140,34 @@ BC.PortletLayout = function() {
   }
 }();
 
+// 'use strict';
+// BC.PortletPricePerPiece = function() {
+//   const msrpPPPInputId = 'ppp-msrp',
+//         userPPPInputId = 'ppp-your-price';
+
+//   let msrpPPP,
+//       userPPP;
+
+//   const update = function update(setData, purchasePrice) {
+//     msrpPPP = document.getElementById(msrpPPPInputId);
+//     userPPP = document.getElementById(userPPPInputId);
+//     const partCount = setData.pcs;
+//     console.log(setData);
+
+//     if (partCount !== null) {
+//       if (setData.msrp !== null) {
+//         msrpPPP.value = BC.Utils.formatCurrency(setData.msrp / partCount) + " per piece";
+//       }
+
+//       userPPP.value = BC.Utils.formatCurrency(purchasePrice / partCount) + " per piece";
+//     }
+//   }
+
+//   return {
+//     update: update
+//   }
+// }();
+
 'use strict';
 BC.PortletPartOutBrickOwl = function() {
   const boPoNewInputId = 'bo-po-new',
@@ -1186,34 +1214,6 @@ BC.PortletPartOutBrickOwl = function() {
     update: update
   }
 }();
-
-// 'use strict';
-// BC.PortletPricePerPiece = function() {
-//   const msrpPPPInputId = 'ppp-msrp',
-//         userPPPInputId = 'ppp-your-price';
-
-//   let msrpPPP,
-//       userPPP;
-
-//   const update = function update(setData, purchasePrice) {
-//     msrpPPP = document.getElementById(msrpPPPInputId);
-//     userPPP = document.getElementById(userPPPInputId);
-//     const partCount = setData.pcs;
-//     console.log(setData);
-
-//     if (partCount !== null) {
-//       if (setData.msrp !== null) {
-//         msrpPPP.value = BC.Utils.formatCurrency(setData.msrp / partCount) + " per piece";
-//       }
-
-//       userPPP.value = BC.Utils.formatCurrency(purchasePrice / partCount) + " per piece";
-//     }
-//   }
-
-//   return {
-//     update: update
-//   }
-// }();
 
 'use strict';
 BC.SetLookupForm = function() {
