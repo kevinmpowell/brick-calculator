58ad184619b76c1188102dcb03c18f5852891c16
Kevin Powell <kevinmpowell@gmail.com>
2018-01-26 07:13:43 -0600

    Add correct size touch icons for iOS

--- docs/scripts/brickulator.js
+++ docs/scripts/brickulator.js
@@ -1140,34 +1140,6 @@ BC.PortletLayout = function() {
   }
 }();
 
-// 'use strict';
-// BC.PortletPricePerPiece = function() {
-//   const msrpPPPInputId = 'ppp-msrp',
-//         userPPPInputId = 'ppp-your-price';
-
-//   let msrpPPP,
-//       userPPP;
-
-//   const update = function update(setData, purchasePrice) {
-//     msrpPPP = document.getElementById(msrpPPPInputId);
-//     userPPP = document.getElementById(userPPPInputId);
-//     const partCount = setData.pcs;
-//     console.log(setData);
-
-//     if (partCount !== null) {
-//       if (setData.msrp !== null) {
-//         msrpPPP.value = BC.Utils.formatCurrency(setData.msrp / partCount) + " per piece";
-//       }
-
-//       userPPP.value = BC.Utils.formatCurrency(purchasePrice / partCount) + " per piece";
-//     }
-//   }
-
-//   return {
-//     update: update
-//   }
-// }();
-
 'use strict';
 BC.PortletPartOutBrickOwl = function() {
   const boPoNewInputId = 'bo-po-new',
@@ -1214,6 +1186,34 @@ BC.PortletPartOutBrickOwl = function() {
     update: update
   }
 }();
+
+// 'use strict';
+// BC.PortletPricePerPiece = function() {
+//   const msrpPPPInputId = 'ppp-msrp',
+//         userPPPInputId = 'ppp-your-price';
+
+//   let msrpPPP,
+//       userPPP;
+
+//   const update = function update(setData, purchasePrice) {
+//     msrpPPP = document.getElementById(msrpPPPInputId);
+//     userPPP = document.getElementById(userPPPInputId);
+//     const partCount = setData.pcs;
+//     console.log(setData);
+
+//     if (partCount !== null) {
+//       if (setData.msrp !== null) {
+//         msrpPPP.value = BC.Utils.formatCurrency(setData.msrp / partCount) + " per piece";
+//       }
+
+//       userPPP.value = BC.Utils.formatCurrency(purchasePrice / partCount) + " per piece";
+//     }
+//   }
+
+//   return {
+//     update: update
+//   }
+// }();
 
 'use strict';
 BC.SetLookupForm = function() {
