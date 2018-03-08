4a09a7b0f5f3ba9b2cd61d32a5109fcad9f200da
Kevin Powell <kevinmpowell@gmail.com>
2018-03-07 22:27:05 -0600

    Add password reset form

--- docs/scripts/subpage_scripts/reset_password_form.js
+++ docs/scripts/subpage_scripts/reset_password_form.js
@@ -2,7 +2,17 @@ var BC = BC || {};
 
 /* globals BC localStorageKeys apiMapping currentDomain customEvents */
 'use strict';
+
+const currentDomain = window.location.hostname,
+apiMapping = {
+  'localhost': 'https://localhost:5000',
+  '10.0.1.15': 'http://10.0.1.15:5000',
+  'kevinmpowell.github.io': 'https://brickulator-api.herokuapp.com'
+},
+apiDomain = apiMapping[currentDomain];
+
 BC.ResetPasswordForm = function() {
+  const successClass = 'bc-reset-password-form--succeeded';
   // function disableForm() {
   //   document.bcForgotPasswordForm.email.setAttribute('disabled', true);
   // }
@@ -50,9 +60,64 @@ BC.ResetPasswordForm = function() {
   //   );
   // }
 
+  function showResetSuccess() {
+    document.bcResetPasswordForm.classList.add(successClass);
+  }
+
+  function showResetError() {
+    alert("Cannot reset password. Contact support for details.");
+  }
+
+  function resetPassword(newPassword, newPasswordConfirm, resetToken) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', apiDomain + '/password/reset?reset_password_token=' + resetToken + '&password=' + newPassword + '&password_confirmation=' + newPasswordConfirm);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        showResetSuccess();
+      } else {
+        showResetError();
+      }
+    };
+    xhr.onerror = function () {
+      alert("")
+    };
+
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.send();
+  }
+
+  function getParameterByName(name, url) {
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+          results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+
+  function handlePasswordResetFormSubmit(e) {
+    e.preventDefault();
+    const newPassword = this.newPassword.value,
+          newPasswordConfirm = this.newPasswordConfirm.value,
+          resetToken = getParameterByName('password_reset_token');
+
+    if (typeof resetToken === 'undefined') {
+      showResetError();
+    } else if (newPassword.length === 0) {
+      alert("Please enter a new password.");
+    } else if (newPassword !== newPasswordConfirm) {
+      alert("Password and password confirmation don't match. Please fix and try again.");
+    } else {
+      resetPassword(newPassword, newPasswordConfirm, resetToken);
+    }
+
+    return false;
+  }
+
   function setEventListeners() {
-    // document.bcForgotPasswordForm.addEventListener("submit", handlePasswordResetFormSubmit);
-    console.log("FOO");
+    document.bcResetPasswordForm.addEventListener("submit", handlePasswordResetFormSubmit);
   }
 
   const initialize = function initialize() {
